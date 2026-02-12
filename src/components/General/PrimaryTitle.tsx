7d261a5df1aa85b22b1028b900a05e4618ce22bd
Michael Lyons <michael.lyons1112@gmail.com>
2026-02-12 12:13:39 +0000

    Implementing basic working title design

--- src/components/General/PrimaryTitle.tsx
+++ src/components/General/PrimaryTitle.tsx
@@ -1,69 +1,59 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 type PrimaryTitleProps = {
   children?: string
-  fontSize?: string | number
-  shadow?: number
-  primaryColour?: string
-  secondaryColour?: string
+  $fontSize?: string
+  $backgroundImage?: string
+  $backgroundSize?: string
 }
 
-const slidingBackground = keyframes`
-  0%, 100% {
-    background-position: 0 40%;
-  }
-
-  50% {
-    background-position: 100% 40%;
-  }
-`;
-
-const StyledTitleWrapper = styled.div`
-  background: url("https://images.unsplash.com/photo-1452697620382-f6543ead73b5") no-repeat left / 120%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100dvh;
-  width: 100dvw;
-
-  animation: ${slidingBackground} 40s linear infinite;
-`;
-
-const generateTextShadow = (layers: number, color: string) =>
-  Array.from({ length: layers }, (_, i) => {
-    const offset = i + 1;
-    return `${offset}px ${offset}px ${color}`;
-  }).join(", ");
-
 const StyledTitle = styled.h1<PrimaryTitleProps>`
-  background: ${({ primaryColour }) => primaryColour};
-  color: ${({ primaryColour }) => primaryColour};
-  font-family: "Montserrat", sans-serif;
-  font-size: ${({ fontSize }) => fontSize};
-  letter-spacing: 1vw;
-  margin: 0;
-  mix-blend-mode: lighten;
+  position: relative;
+  font-size: ${({ $fontSize }) => $fontSize};
   text-transform: uppercase;
-  text-shadow: ${({ shadow, secondaryColour }) => 
-    `0 0 0px, ${generateTextShadow(Number(shadow), secondaryColour || 'black')}`};
+  text-align: center;
 
-  &:before {
-    color: ${({ primaryColour }) => primaryColour};
+  color: black;
+  @supports (background-clip: text) or (-webkit-background-clip: text) {
+    background-image: 
+      linear-gradient(0deg, rgba(28, 23, 158, 0.3), rgba(41, 219, 210, 0.1)), 
+      url("${({ $backgroundImage }) => $backgroundImage}");
+    background-size: ${({ $backgroundSize }) => $backgroundSize};
+    background-position: center;
+
+    color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-stroke: 1px white;
+
+    animation: slideBG 10s linear infinite;
+
+    @keyframes slideBG {
+      0% { background-position: 0 0; }
+      50% { background-position: 100% 50%; }
+      100% { background-position: 0 0; }
+    }
+  }
+
+  // Separate shadow on lower z-index, preventing issue with transparent text effect
+  &::before{
     content: attr(data-text);
-    margin-left: -1%;
-    margin-top: -1%;
     position: absolute;
+    top: 0; left: 0;
+    width: 100%; height: 100%;
+    color: transparent;
+    z-index: -1; /* behind the real text */
+    text-shadow: 2px 2px 5px rgba(0,0,0,0.5); /* solid shadow behind letters */
   }
 `;
 
-const PrimaryTitle = ({ children, fontSize, shadow, primaryColour, secondaryColour } : PrimaryTitleProps) => {
+// Main title, includes animated background
+const PrimaryTitle = ({ children, $fontSize, $backgroundImage, $backgroundSize } : PrimaryTitleProps) => {
   return (
-    <StyledTitleWrapper>
-      <StyledTitle fontSize={fontSize} shadow={shadow} primaryColour={primaryColour} secondaryColour={secondaryColour}>
-        {children}
-      </StyledTitle>
-    </StyledTitleWrapper>
-  )
+    <StyledTitle data-text={children} $fontSize={$fontSize} $backgroundImage={$backgroundImage} $backgroundSize={$backgroundSize}>
+      {children}
+    </StyledTitle>
+  );
 }
 
 export default PrimaryTitle
