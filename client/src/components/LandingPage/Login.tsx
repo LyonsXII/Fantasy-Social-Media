81b7b5d70cf86445f41cbe47f69322ad01e7f8e1
Michael Lyons <michael.lyons1112@gmail.com>
2026-02-14 21:11:17 +0000

    Finishing basic login / register functionality

--- client/src/components/LandingPage/Login.tsx
+++ client/src/components/LandingPage/Login.tsx
@@ -6,10 +6,11 @@ import axios from "axios";
 import LoginIcon from "../../assets/icons/login.svg?react";
 import PasswordIcon from "../../assets/icons/password.svg?react";
 import EyeIcon from "../../assets/icons/eye.svg?react";
+import NewUserIcon from "../../assets/icons/new.svg?react";
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-type StyledButtonProps = {
+type StyledLoginButton = {
   $expanded: boolean
   $heightAdjust: number
 }
@@ -18,6 +19,16 @@ type LoginProps = {
   login: string;
   password: string;
 };
+
+const StyledContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  height: fit-content;
+  width: 40dvw;
+  padding: 10px 20px;
+`;
 
 const StyledLoginWrapper = styled.div`
   display: flex;
@@ -68,7 +79,7 @@ const StyledField = styled.div<{$visible: boolean}>`
 const StyledInput = styled.input`
   width: 100%;
   outline: none;
-  border: 1px solid yellow;
+  border: none;
 
   &::placeholder {
     color: hsl(0deg 0% 0%);
@@ -76,12 +87,50 @@ const StyledInput = styled.input`
   } 
 `
 
+const StyledNewUserWrapper = styled.div<{ $visible : boolean}>`
+  display: ${({ $visible }) => $visible ? "flex" : "none"};
+  align-items: center;
+  height: 200px;
+
+  transform-origin: top;
+  transform: ${({ $visible }) => $visible ? "scaleY(1)" : "scaleY(0)"};
+  transition: transform 0.8s ease;
+
+  ${({ $visible }) =>
+    $visible &&
+    css`
+      animation: ${expandLogin} 1s ease-out forwards;
+    `}
+`;
+
+const StyledNewUserButton = styled.button<{ $expanded : boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  height: fit-content;
+  width: 140px;
+  padding: 1em;
+  background: hsl(233deg 36% 38%);
+  color: hsl(0 0 100);
+  border: none;
+  border-radius: 30px;
+  font-weight: 600;
+  cursor: pointer;
+
+  transition: width 0.8s ease, transform 0.2s ease;
+
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
+
 const createStyledIcon = (IconComponent: ComponentType<SVGProps<SVGSVGElement>>) => styled(IconComponent)`
   height: 20px;
   width: 20px;
-  transition: transform 0.4s;
   cursor: pointer;
   vertical-align: bottom;
+  transition: transform 0.4s;
 
   &:hover {
       transform: scale(1.1);
@@ -91,20 +140,23 @@ const createStyledIcon = (IconComponent: ComponentType<SVGProps<SVGSVGElement>>)
 const StyledLoginIcon = createStyledIcon(LoginIcon);
 const StyledPasswordIcon = createStyledIcon(PasswordIcon);
 const StyledEyeIcon = createStyledIcon(EyeIcon);
+const StyledNewUserIcon = createStyledIcon(NewUserIcon);
 
 // Avoiding relayouts by using wrapper preset to max button width
-const StyledButtonWrapper = styled.div<StyledButtonProps>`
+const StyledLoginButtonWrapper = styled.div<StyledLoginButton>`
   margin-top: ${({ $heightAdjust }) => $heightAdjust ? `-${$heightAdjust}px` : "0px"};
   display: flex;
   justify-content: center;
   height: fit-content;
-  width: 200px;
+  padding: 10px 0px;
+  gap: 10px;
+  width: 240px;
 
   transition: transform 0.8s ease;
   transform: ${({ $expanded, $heightAdjust }) => $expanded ? `translateY(${$heightAdjust}px)` : "translateY(0px)"};
 `
 
-const StyledButton = styled.button<StyledButtonProps>`
+const StyledLoginButton = styled.button<StyledLoginButton>`
   width: ${({ $expanded }) => $expanded ? "200px" : "100px"};
   padding: 1em;
   background: hsl(233deg 36% 38%);
@@ -114,7 +166,11 @@ const StyledButton = styled.button<StyledButtonProps>`
   font-weight: 600;
   cursor: pointer;
 
-  transition: width 0.8s ease;
+  transition: width 0.8s ease, transform 0.2s ease;
+
+  &:hover {
+    transform: scale(1.02);
+  }
 `
 
 const Login = () => {
@@ -122,6 +178,7 @@ const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
   const fieldsHeightRef = useRef<HTMLDivElement>(null);
   const [fieldsHeight, setFieldsHeight] = useState<number>(0)
+  const [submitMode, setSubmitMode] = useState<string>("login");
 
   const [details, setDetails] = useState<{ login: string; password: string }>({ 
     "login": "", 
@@ -148,6 +205,22 @@ const Login = () => {
 
   function togglePasswordVisible() {
     setPasswordVisible(prev => !prev);
+  };
+
+  function toggleSubmitMode() {
+    if (submitMode == "login") {
+      setSubmitMode("register");
+    } else {
+      setSubmitMode("login");
+    }
+  };
+
+  async function handleMode() {
+    if (submitMode === "login") {
+      await login();
+    } else {
+      await register();
+    }
   };
 
   async function register() {
@@ -184,24 +257,33 @@ const Login = () => {
   }, [showLogin]);
 
   return (
-    <StyledLoginWrapper>
-      <StyledFieldsWrapper $visible={showLogin}>
-        <StyledField $visible={showLogin}>
-          <StyledLoginIcon/>
-          <StyledInput type="text" name="login" value={details.login} placeholder="Login" onChange={((e) => updateField("login", e.target.value))}/>
-        </StyledField>
-        <StyledField ref={fieldsHeightRef} $visible={showLogin}>
-          <StyledPasswordIcon/>
-          <StyledInput type={passwordVisible ? "text" : "password"} name="email" value={details.password} placeholder="Password" onChange={((e) => updateField("password", e.target.value))}/>
-          <StyledEyeIcon onClick={togglePasswordVisible}/>
-        </StyledField>
-      </StyledFieldsWrapper>
-      <StyledButtonWrapper $expanded={showLogin} $heightAdjust={fieldsHeight}>
-        <StyledButton onClick={showLogin ? register : toggleLoginVisible} $expanded={showLogin} $heightAdjust={fieldsHeight}>
-          Login
-        </StyledButton>
-      </StyledButtonWrapper>
-    </StyledLoginWrapper>
+    <StyledContentWrapper>
+      <StyledLoginWrapper>
+        <StyledFieldsWrapper $visible={showLogin}>
+          <StyledField $visible={showLogin}>
+            <StyledLoginIcon/>
+            <StyledInput type="text" name="login" value={details.login} placeholder="Login" onChange={((e) => updateField("login", e.target.value))}/>
+          </StyledField>
+          <StyledField ref={fieldsHeightRef} $visible={showLogin}>
+            <StyledPasswordIcon/>
+            <StyledInput type={passwordVisible ? "text" : "password"} name="email" value={details.password} placeholder="Password" onChange={((e) => updateField("password", e.target.value))}/>
+            <StyledEyeIcon onClick={togglePasswordVisible}/>
+          </StyledField>
+        </StyledFieldsWrapper>
+        <StyledLoginButtonWrapper $expanded={showLogin} $heightAdjust={fieldsHeight}>
+          <StyledLoginButton onClick={showLogin ? handleMode : toggleLoginVisible} $expanded={showLogin} $heightAdjust={fieldsHeight}>
+            {submitMode == "login" ? "Login" : "Register"}
+          </StyledLoginButton>
+        </StyledLoginButtonWrapper>
+      </StyledLoginWrapper>
+
+      <StyledNewUserWrapper $visible={showLogin}>
+        <StyledNewUserButton onClick={toggleSubmitMode} $expanded={showLogin}>
+          New User?
+          <StyledNewUserIcon/>
+        </StyledNewUserButton>
+      </StyledNewUserWrapper>
+    </StyledContentWrapper>
   )
 }
 
